2812939c68d05950d391b3844922572699636ec1
Arun Udayakumar <arunu2002@gmail.com>
2018-04-23 11:58:17 +0530

    Update scoperesolution.cpp

--- scoperesolution.cpp
+++ scoperesolution.cpp
@@ -9,11 +9,11 @@ int main()
 int assign = 500;
 {
 int assign = 5000;
-cout << "assign " << assign << '\n';
-cout << "::a " << ::assign << '\n';
+cout << "assign " << assign << '\n'; //assign = 5000
+cout << "::assign " << ::assign << '\n';  //assign = 50
 }
-cout << "assign " << assign << '\n';
-cout << "::a " << ::assign << '\n';
+cout << "assign " << assign << '\n'; //assign = 500
+cout << "::assign " << ::assign << '\n';  //assign = 50
 
 
 }
